a728e768c65930438cff05047d9a6d5b496c1646
Pratheesh <116364415+pratheeshsailor@users.noreply.github.com>
2025-07-04 12:51:13 +0530

    task5 to task7

--- Day14/src/StackExample.java
+++ Day14/src/StackExample.java
@@ -9,17 +9,25 @@ public class StackExample {
         stack.push(10);
         stack.push(20);
         stack.push(30);
+        stack.push(40);
 
         // Display the stack
         System.out.println("Stack: " + stack);
 
         // Pop the top element
         int poppedElement = stack.pop();
+        int peekElement = stack.peek(40);  // returns an object from the top of the stack
+        int position = stack.search(20);  //returns the position of the element from the top of the stack
+        boolean result = stack.empty();  //whether a stack is empty or not
 
         // Print the popped element
         System.out.println("Popped element: " + poppedElement);
 
         // Display the stack after pop
         System.out.println("Stack after pop: " + stack);
+        System.out.println("Stack after peek: " + peekElement);
+        System.out.println("Stack searching : " + position);
+        System.out.println("Stack searching : " + result);
+
     }
 }
